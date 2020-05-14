552bc004f2ebbc192ff33b4351c65e7e654744be
rekhin.nvs <rekhin.nvs@gmail.com>
2020-05-14 23:29:11 +0530

    Initialise Chrome driver.

---
+++ src/main/java/utils/Base.java
@@ -0,0 +1,19 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
+
+public class Base {
+
+    static WebDriver driver;
+    public static WebDriver initializeDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        return driver;
+    }
+}
