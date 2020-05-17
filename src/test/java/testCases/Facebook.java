3df15181097bb31c47f1572e05a5fe8346e8e8e9
rekhin.nvs <rekhin.nvs@gmail.com>
2020-05-17 21:00:59 +0530

    Add login for facebook

---
+++ src/test/java/testCases/Facebook.java
@@ -0,0 +1,45 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Guice;
+import org.testng.annotations.Test;
+import utils.Base;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Facebook {
+    static WebDriver driver;
+
+    @BeforeTest
+    public void initDriver() {
+        System.out.println("Initializing driver");
+        driver = Base.initializeDriver();
+    }
+    @Test
+    public void launchChrome() {
+
+        driver.get("https://facebook.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String title = driver.getTitle();
+        System.out.println(title);
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test(enabled = false)
+    public void loginToFacebook() {
+        //xpath syntax //tagname[@attribute='value']
+        driver.findElement(By.id("email")).sendKeys("dracoz");
+        driver.findElement(By.id("pass")).sendKeys("kukkoos");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+    }
+
+    @Test
+    public void loginByCss() {
+        driver.findElement(By.cssSelector("input[name='email']")).sendKeys("hellow");
+        driver.findElement(By.cssSelector("input[id='pass']")).sendKeys("hereIam");
+        driver.findElement(By.cssSelector("input[value='Log In']")).click();
+    }
+
+}
