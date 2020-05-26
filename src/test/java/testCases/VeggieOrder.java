86f6da02dd49d2eae733e3a48e914c7a9454d0c6
rekhin.nvs <rekhin.nvs@gmail.com>
2020-05-26 22:41:58 +0530

    Add class for ordering veggies.

---
+++ src/test/java/testCases/VeggieOrder.java
@@ -0,0 +1,53 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.Base;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class VeggieOrder {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public void  initDriver() throws InterruptedException {
+        driver = Base.initializeDriver();
+        driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/");
+        Thread.sleep(5000);
+    }
+
+    @Test(dataProvider="veggieNames", priority = 1)
+    public void orderVeggies(String vegitableName) throws InterruptedException {
+
+        List<WebElement> veggieNames = driver.findElements(By.cssSelector("h4.product-name"));
+
+        for(int i=0; i<veggieNames.size();i++) {
+            System.out.println(veggieNames.get(i).getText());
+            if(veggieNames.get(i).getText().contains(vegitableName)) {
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+                break;
+            }
+        }
+        Thread.sleep(1000);
+
+    }
+
+    @Test(priority = 2)
+    public void checkOut() {
+        driver.findElement(By.cssSelector("a.cart-icon")).click();
+        driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+    }
+
+    @DataProvider
+    public Object[] veggieNames() {
+        Object[] vegitables = {"Cucumber","Carrot", "Brocolli"};
+
+        return vegitables;
+    }
+}
