54fec039b562c1a0364fc3b9fb7b1c2fdc93cf9d
rekhin.nvs <rekhin.nvs@gmail.com>
2020-05-28 20:41:17 +0530

    complete assignemnt2

--- src/test/java/testCases/Assignemens.java
+++ src/test/java/testCases/Assignemens.java
@@ -3,13 +3,16 @@ package testCases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import utils.Base;
 
 import java.sql.Driver;
+import java.time.Duration;
 
-public class Assignement2 {
+public class Assignemens {
 
     static WebDriver driver;
 
@@ -19,7 +22,7 @@ public class Assignement2 {
         driver.get("https://www.cleartrip.com/");
     }
 
-    @Test
+    @Test(description = "Assignment 2",enabled = false)
     public void searchForFlights() throws  InterruptedException{
         driver.get("https://www.cleartrip.com/");
         driver.findElement(By.id("DepartDate")).click();
@@ -38,4 +41,14 @@ public class Assignement2 {
         System.out.println(errorMessage);
 
     }
+
+    @Test(description = "Assignment 3")
+    public void explicityWait() {
+        driver.get("http://www.itgeared.com/demo/1506-ajax-loading.html");
+        driver.findElement(By.xpath("//div[@id='content']/a[2]")).click();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
+        System.out.println(driver.findElement(By.id("results")));
+    }
+
 }
--- src/test/java/testCases/VeggieOrder.java
+++ src/test/java/testCases/VeggieOrder.java
@@ -3,23 +3,30 @@ package testCases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.Base;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class VeggieOrder extends Base{
 
     static WebDriver driver;
+    WebDriverWait webDriverWait;
 
     @BeforeClass
     public void  initDriver() throws InterruptedException {
         driver = Base.initializeDriver();
         driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/");
         implicitWaitFor(5);
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @Test(dataProvider="veggieNames", priority = 1)
@@ -42,6 +49,22 @@ public class VeggieOrder extends Base{
     public void checkOut() {
         driver.findElement(By.cssSelector("a.cart-icon")).click();
         driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+    }
+
+    @Test(priority = 3)
+    public void placeOrder(){
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promoBtn")).click();
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+        driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+
+    }
+
+    @Test(priority = 4)
+    public void confirmOrder() {
+        //Select select = new Select();
+
     }
 
     @DataProvider
