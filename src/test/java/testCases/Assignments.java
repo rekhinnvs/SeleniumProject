10525a792656a56c28de8062bd02e7e27e9c15d1
rekhin.nvs <rekhin.nvs@gmail.com>
2020-05-30 21:23:11 +0530

    Add fluentwait test case

--- src/test/java/testCases/Assignments.java
+++ src/test/java/testCases/Assignments.java
@@ -1,17 +1,17 @@
 package testCases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utils.Base;
 
 import java.sql.Driver;
 import java.time.Duration;
+import java.util.function.Function;
 
 public class Assignments {
 
@@ -42,13 +42,36 @@ public class Assignments {
 
     }
 
-    @Test(description = "Assignment 3")
+    @Test(description = "Assignment 3",enabled = false)
     public void explicityWait() {
         driver.get("http://www.itgeared.com/demo/1506-ajax-loading.html");
         driver.findElement(By.xpath("//div[@id='content']/a[2]")).click();
         WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
         System.out.println(driver.findElement(By.id("results")));
+    }
+
+    @Test(description = "Fluent wait")
+    public void fluentWait() {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(4))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                if(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
+                    System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
+                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
+                }
+                else
+                    return null;
+            }
+        });
+
     }
 
 }
