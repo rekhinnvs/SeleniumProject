8d26d0d81d167711bc7bef20833140eed9e2d0e5
rekhin.nvs <rekhin.nvs@gmail.com>
2020-07-10 16:41:50 +0530

    Selenium4 switch to tab and get values

--- src/test/java/testCases/Selenium4.java
+++ src/test/java/testCases/Selenium4.java
@@ -3,11 +3,15 @@ package testCases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utils.Base;
+
+import java.util.Iterator;
+import java.util.Set;
 
 import static org.openqa.selenium.support.locators.RelativeLocator.withTagName;
 
@@ -22,18 +26,32 @@ public class Selenium4 extends Base {
         context.setAttribute("WebDriver", driver);
     }
 
-    @Test
+    @Test(enabled = false)
     public void aboveOfElement() {
         driver.get("https://rahulshettyacademy.com/angularpractice/");
         WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
         System.out.println(driver.findElement(withTagName("label").above(nameEditBox)).getText());
     }
 
+    @Test(description = "Switch to a tab and get a value from it")
+    public void newTab() {
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        WebElement name = driver.findElement(By.name("name"));
+        driver.switchTo().newWindow(WindowType.TAB);
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> iter = windowHandles.iterator();
+        String parentWindow = iter.next();
+        String childWindow = iter.next();
+        driver.switchTo().window(childWindow);
+        driver.get("https://rahulshettyacademy.com/#/index");
+        int numberOfLinks = driver.findElements(By.tagName("a")).size();
+        System.out.println(numberOfLinks);
+        driver.switchTo().window(parentWindow);
+        name.sendKeys("" + numberOfLinks);
+    }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
-
-
 }
