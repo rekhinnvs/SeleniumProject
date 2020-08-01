da92d08d07554db5da16700fa56a8898cf56f45c
rekhin.nvs <rekhin.nvs@gmail.com>
2020-08-01 10:31:56 +0530

    Find the number of broken links

--- src/test/java/interviewPrep/Class_1.java
+++ src/test/java/interviewPrep/Class_1.java
@@ -1,5 +1,73 @@
 package interviewPrep;
 
-public class Class_1 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.Base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+public class Class_1 extends Base {
+    WebDriver driver;
+    Properties properties;
+
+    @BeforeClass
+    public void setup() throws IOException {
+        driver = initializeDriver();
+        implicitWaitFor(3);
+        properties = new Properties();
+        FileInputStream fis = new FileInputStream("./src/data.properties");
+        properties.load(fis);
+    }
+
+    @Test
+    public void findBrokenLinks() {
+        String url = "";
+        HttpURLConnection huc = null;
+        int respCode = 200;
+        String baseUrl = properties.getProperty("rahulShetty");
+        driver.get(baseUrl);
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("Number of links " + links.size());
+        for (WebElement link : links) {
+            url = link.getAttribute("href");
+            //System.out.println(url);
+            if (url == null || url.isEmpty()) {
+                //System.out.println(link.getAttribute());
+                System.out.println("Url is either null or empty");
+                continue;
+            }
+            if (!url.startsWith(baseUrl)) {
+                System.out.println("URL belongs to another domain, skipping it.");
+                continue;
+            }
+            try {
+                huc = (HttpURLConnection) (new URL(url).openConnection());
+                huc.setRequestMethod("HEAD");
+                huc.connect();
+                respCode = huc.getResponseCode();
+                if (respCode >= 400) {
+                    System.out.println(url + " is a broken link");
+                } else {
+                    System.out.println(url + " is a valid link");
+                }
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
